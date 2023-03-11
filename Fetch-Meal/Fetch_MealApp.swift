1f21c5816684aabca04236e5b6259f0950c6a441
pavan3008 <pavansai3008@gmail.com>
2023-03-10 17:36:41 -0800

    Initial Commit

---
+++ Fetch-Meal/Fetch_MealApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_MealApp.swift
+//  Fetch-Meal
+//
+//  Created by Pavan Sai Nallagoni on 3/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_MealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
